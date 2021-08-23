00f25b54ac00424a804efed462899c9ceb8de679
tangzeyu <1213280733@qq.com>
2021-08-23 09:42:48 +0800

    8.23 f2

--- src/day09/tostring/Test9.java
+++ src/day09/tostring/Test9.java
@@ -4,9 +4,9 @@ public class Test9{
 	
 	public static void main(String[] args) {
 		
-		String s1="bwf";
+		String s1="qwer";
 		
-		String s2=new String("51testing");
+		String s2=new String("qwer");
 		
 		System.out.println(s1);
 		System.out.println(s2);
