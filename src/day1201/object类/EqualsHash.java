3d8f2664f779b0b2c573784a18eabf05338f1d63
tangzeyu <1213280733@qq.com>
2021-08-25 16:33:39 +0800

    8.25 pm final

---
+++ src/day1201/object类/EqualsHash.java
@@ -0,0 +1,40 @@
+package day1201.object类;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class EqualsHash {
+
+	@Override
+	public int hashCode() {
+		return 1;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		EqualsHash t = new EqualsHash();
+		System.out.println(t);
+		System.out.println(t.hashCode());
+		System.out.println(Integer.toHexString(t.hashCode()));
+
+		EqualsHash t2 = new EqualsHash();
+		System.out.println(t2.hashCode());
+		System.out.println(t.equals(t2));
+		System.out.println(t == t2);
+		
+		Set set=new HashSet();
+		set.add(t);
+		set.add(t2);
+		System.out.println(set.size());
+		
+		
+		
+	}
+
+}
