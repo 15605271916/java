f47117bf9d691232bad457323e9c2bfbbf1fefef
tangzeyu <1213280733@qq.com>
2021-10-18 09:40:36 +0800

    10.18 v1

---
+++ src/day101102J2EE16redis03缓存技术03/User.java
@@ -0,0 +1,29 @@
+package day101102J2EE16redis03缓存技术03;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+	
+	private int id;
+	
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
